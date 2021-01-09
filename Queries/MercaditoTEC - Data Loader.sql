5083bc6a3ddf5fa946a04861e581a0e2465c0ed8
Rubén Salas <rubsalas@hotmail.com>
2021-01-09 04:38:43 -0600

    Create MercaditoTEC - Data Loader.sql

---
+++ Queries/MercaditoTEC - Data Loader.sql
@@ -0,0 +1,131 @@
+
+/*
+--------------------------------------------------------------------
+MercaditoTEC
+--------------------------------------------------------------------
+Instituto Técnológico de Costa Rica
+Área Académica de Ingeniería en Computadores
+Especificación y Diseño de Software (CE4101) 
+II Semestre 2020
+Prof. Luis Diego Noguera Mena
+Cristhofer Azofeifa, Tatiana Mora & Rubén Salas
+--------------------------------------------------------------------
+MercaditoTEC - Data Loader
+--------------------------------------------------------------------
+*/
+
+
+/*
+--------------------------------------------------------------------
+Ubicacion de Inserts por Tabla en Script Actual
+--------------------------------------------------------------------
+Persona...................................................31
+--------------------------------------------------------------------
+*/
+
+
+USE [MercaditoTEC]
+GO
+
+
+/****** Data:  Persona ******/
+insert into Persona (nombre, apellidos, telefono) values ('Sibel', 'Gamage', '5417-7876');
+insert into Persona (nombre, apellidos, telefono) values ('Isaiah', 'Blabie', '1872-2960');
+insert into Persona (nombre, apellidos, telefono) values ('Elle', 'Timbs', '3314-4624');
+insert into Persona (nombre, apellidos, telefono) values ('Sheilah', 'Marvell', '1798-8012');
+insert into Persona (nombre, apellidos, telefono) values ('Madella', 'Bodicum', '8341-1082');
+insert into Persona (nombre, apellidos, telefono) values ('Alikee', 'Quantick', '1999-9213');
+insert into Persona (nombre, apellidos, telefono) values ('Addy', 'Pencot', '3351-1976');
+insert into Persona (nombre, apellidos, telefono) values ('Mervin', 'Colquitt', '8022-2089');
+insert into Persona (nombre, apellidos, telefono) values ('Gui', 'Hayford', '2599-9053');
+insert into Persona (nombre, apellidos, telefono) values ('Farley', 'Tebbe', '1478-8888');
+insert into Persona (nombre, apellidos, telefono) values ('Shay', 'Hatherley', '4085-5256');
+insert into Persona (nombre, apellidos, telefono) values ('Jeffry', 'Corriea', '4028-8120');
+insert into Persona (nombre, apellidos, telefono) values ('Abbott', 'Frango', '7081-1952');
+insert into Persona (nombre, apellidos, telefono) values ('Tobit', 'Ledner', '2341-1267');
+insert into Persona (nombre, apellidos, telefono) values ('Daveta', 'Klulik', '7865-5884');
+insert into Persona (nombre, apellidos, telefono) values ('Natka', 'Daines', '8652-2035');
+insert into Persona (nombre, apellidos, telefono) values ('Timi', 'Murrthum', '7832-2401');
+insert into Persona (nombre, apellidos, telefono) values ('Maisey', 'Swires', '8624-4693');
+insert into Persona (nombre, apellidos, telefono) values ('Venita', 'Crosfield', '8765-5268');
+insert into Persona (nombre, apellidos, telefono) values ('Shela', 'Quarry', '4023-3477');
+insert into Persona (nombre, apellidos, telefono) values ('Adeline', 'Weatherup', '1043-3952');
+insert into Persona (nombre, apellidos, telefono) values ('Sallee', 'Leveritt', '5121-1117');
+insert into Persona (nombre, apellidos, telefono) values ('Daile', 'Hissett', '5754-4156');
+insert into Persona (nombre, apellidos, telefono) values ('Trstram', 'd''Arcy', '3459-9026');
+insert into Persona (nombre, apellidos, telefono) values ('Mickie', 'Camidge', '3692-2692');
+insert into Persona (nombre, apellidos, telefono) values ('Morgen', 'Westall', '3615-5137');
+insert into Persona (nombre, apellidos, telefono) values ('Fulton', 'Lockyear', '7145-5146');
+insert into Persona (nombre, apellidos, telefono) values ('Mylo', 'Pothecary', '3059-9324');
+insert into Persona (nombre, apellidos, telefono) values ('Francis', 'Flukes', '9849-9422');
+insert into Persona (nombre, apellidos, telefono) values ('Correy', 'Yankov', '6077-7235');
+insert into Persona (nombre, apellidos, telefono) values ('Pall', 'De Francesco', '5461-1286');
+insert into Persona (nombre, apellidos, telefono) values ('Berthe', 'Benger', '3488-8921');
+insert into Persona (nombre, apellidos, telefono) values ('Fredrika', 'McCaughen', '9311-1680');
+insert into Persona (nombre, apellidos, telefono) values ('Tybi', 'Alderwick', '2175-5746');
+insert into Persona (nombre, apellidos, telefono) values ('Vivi', 'Mordey', '5567-7084');
+insert into Persona (nombre, apellidos, telefono) values ('Cherri', 'Jervois', '5945-5239');
+insert into Persona (nombre, apellidos, telefono) values ('Rhys', 'Lathom', '7955-5007');
+insert into Persona (nombre, apellidos, telefono) values ('Burt', 'Ghidoli', '7791-1562');
+insert into Persona (nombre, apellidos, telefono) values ('Mariel', 'Braidman', '9491-1719');
+insert into Persona (nombre, apellidos, telefono) values ('Arleen', 'Litster', '5307-7112');
+insert into Persona (nombre, apellidos, telefono) values ('Indira', 'Thouless', '5497-7947');
+insert into Persona (nombre, apellidos, telefono) values ('Marti', 'Syncke', '4459-9996');
+insert into Persona (nombre, apellidos, telefono) values ('Shanon', 'Kornyshev', '3259-9180');
+insert into Persona (nombre, apellidos, telefono) values ('Kellby', 'Aupol', '1949-9234');
+insert into Persona (nombre, apellidos, telefono) values ('Hershel', 'Lowrey', '2029-9904');
+insert into Persona (nombre, apellidos, telefono) values ('Rea', 'Budibent', '8766-6008');
+insert into Persona (nombre, apellidos, telefono) values ('Mordecai', 'Addington', '5302-2441');
+insert into Persona (nombre, apellidos, telefono) values ('Abey', 'Brosi', '3105-5020');
+insert into Persona (nombre, apellidos, telefono) values ('Gary', 'Plumb', '6113-3717');
+insert into Persona (nombre, apellidos, telefono) values ('Bronny', 'Boissier', '9638-8257');
+insert into Persona (nombre, apellidos, telefono) values ('Free', 'Katz', '3697-7856');
+insert into Persona (nombre, apellidos, telefono) values ('Odetta', 'Kilmary', '3453-3176');
+insert into Persona (nombre, apellidos, telefono) values ('Kermit', 'Low', '4363-3485');
+insert into Persona (nombre, apellidos, telefono) values ('Chandler', 'Simoncelli', '7716-6490');
+insert into Persona (nombre, apellidos, telefono) values ('Mellie', 'Roast', '1251-1416');
+insert into Persona (nombre, apellidos, telefono) values ('Rosco', 'Flement', '6621-1434');
+insert into Persona (nombre, apellidos, telefono) values ('Kenyon', 'Morland', '7271-1508');
+insert into Persona (nombre, apellidos, telefono) values ('Chrystel', 'MacGillespie', '5673-3521');
+insert into Persona (nombre, apellidos, telefono) values ('Petronilla', 'MacAlpyne', '6627-7720');
+insert into Persona (nombre, apellidos, telefono) values ('Adaline', 'Mirfin', '6437-7860');
+insert into Persona (nombre, apellidos, telefono) values ('Ludovico', 'Lambshine', '8767-7977');
+insert into Persona (nombre, apellidos, telefono) values ('Tiena', 'Steele', '9496-6950');
+insert into Persona (nombre, apellidos, telefono) values ('Rosalinda', 'Fittis', '2905-5803');
+insert into Persona (nombre, apellidos, telefono) values ('Mirilla', 'Standon', '2987-7253');
+insert into Persona (nombre, apellidos, telefono) values ('Shelley', 'Urlin', '3893-3157');
+insert into Persona (nombre, apellidos, telefono) values ('Rice', 'Teasey', '2566-6872');
+insert into Persona (nombre, apellidos, telefono) values ('Newton', 'McAnulty', '9268-8722');
+insert into Persona (nombre, apellidos, telefono) values ('Cesaro', 'Wickersham', '8767-7498');
+insert into Persona (nombre, apellidos, telefono) values ('Ephraim', 'Tabard', '8698-8393');
+insert into Persona (nombre, apellidos, telefono) values ('Gene', 'Piscot', '6471-1595');
+insert into Persona (nombre, apellidos, telefono) values ('Ellen', 'Odlin', '5742-2028');
+insert into Persona (nombre, apellidos, telefono) values ('Indira', 'Goodhall', '8672-2653');
+insert into Persona (nombre, apellidos, telefono) values ('Aubert', 'Ceresa', '5779-9582');
+insert into Persona (nombre, apellidos, telefono) values ('Rhodie', 'Itzik', '1997-7109');
+insert into Persona (nombre, apellidos, telefono) values ('Rosene', 'MacAulay', '3983-3891');
+insert into Persona (nombre, apellidos, telefono) values ('Marty', 'Jerrems', '6798-8652');
+insert into Persona (nombre, apellidos, telefono) values ('Evangelia', 'Shuttell', '1749-9377');
+insert into Persona (nombre, apellidos, telefono) values ('Joni', 'Adamik', '2089-9903');
+insert into Persona (nombre, apellidos, telefono) values ('Ethelda', 'Adan', '8588-8215');
+insert into Persona (nombre, apellidos, telefono) values ('Germain', 'Pauluzzi', '2315-5172');
+insert into Persona (nombre, apellidos, telefono) values ('Melina', 'Langcaster', '4409-9369');
+insert into Persona (nombre, apellidos, telefono) values ('Reggy', 'Kovalski', '5884-4239');
+insert into Persona (nombre, apellidos, telefono) values ('Allan', 'Jouning', '4446-6466');
+insert into Persona (nombre, apellidos, telefono) values ('Clark', 'Suero', '3512-2652');
+insert into Persona (nombre, apellidos, telefono) values ('Lory', 'Selvey', '8859-9405');
+insert into Persona (nombre, apellidos, telefono) values ('Ryun', 'Brumby', '9048-8926');
+insert into Persona (nombre, apellidos, telefono) values ('Nat', 'Pie', '8056-6760');
+insert into Persona (nombre, apellidos, telefono) values ('Byram', 'Ayto', '9842-2341');
+insert into Persona (nombre, apellidos, telefono) values ('Saxon', 'Cudmore', '7285-5803');
+insert into Persona (nombre, apellidos, telefono) values ('Mallissa', 'Vercruysse', '6344-4340');
+insert into Persona (nombre, apellidos, telefono) values ('Burnaby', 'Thonason', '4284-4701');
+insert into Persona (nombre, apellidos, telefono) values ('Loy', 'Malek', '8909-9084');
+insert into Persona (nombre, apellidos, telefono) values ('Ozzy', 'Belson', '3805-5400');
+insert into Persona (nombre, apellidos, telefono) values ('Kara-lynn', 'Tudor', '8427-7936');
+insert into Persona (nombre, apellidos, telefono) values ('Abraham', 'Synder', '8726-6362');
+insert into Persona (nombre, apellidos, telefono) values ('Lizbeth', 'Solon', '3767-7031');
+insert into Persona (nombre, apellidos, telefono) values ('Claudianus', 'Dunnet', '8387-7155');
+insert into Persona (nombre, apellidos, telefono) values ('Horatius', 'Kinig', '8536-6888');
+insert into Persona (nombre, apellidos, telefono) values ('Cyb', 'Fancet', '4552-2514');
+insert into Persona (nombre, apellidos, telefono) values ('Kirstin', 'Tutton', '2421-1886');
