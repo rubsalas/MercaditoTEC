dfceaf935c05a10dcf4bf152d2263fa97f66690a
Rubén Salas <rubsalas@hotmail.com>
2021-01-30 06:43:37 -0600

    UnitTestProyect

---
+++ UT/Infrastructure/MercaditoTECTestBase.cs
@@ -0,0 +1,33 @@
+﻿using API_MercaditoTEC.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XUnitTest_MercaditoTEC.Infrastructure
+{
+    class MercaditoTECTestBase : IDisposable
+    {
+        private readonly MercaditoTECContext _context;
+
+        public MercaditoTECTestBase()
+        {
+            var options = new DbContextOptionsBuilder<MercaditoTECContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new MercaditoTECContext(options);
+
+            _context.Database.EnsureCreated();
+
+            //MercaditoTECInitializer.Initialize(_context);
+        }
+
+        public void Dispose()
+        {
+            _context.Database.EnsureDeleted();
+
+            _context.Dispose();
+        }
+    }
+}
---
+++ UT/UnitTestDatic.cs
@@ -0,0 +1,111 @@
+using API_MercaditoTEC.Controllers;
+using API_MercaditoTEC.Data;
+using API_MercaditoTEC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using XUnitTest_MercaditoTEC.Infrastructure;
+
+namespace XUnitTest_API_MercaditoTEC
+{
+    public class UnitTestDatic : IDisposable
+    {
+
+        protected readonly MercaditoTECContext _context;
+
+        public UnitTestDatic()
+        {
+            var options = new DbContextOptionsBuilder<MercaditoTECContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new MercaditoTECContext(options);
+
+            _context.Database.EnsureCreated();
+
+            var personas = new[]
+            {
+                new Persona {nombre = "Ruben"},
+                new Persona {nombre = "Tatiana"},
+                new Persona {nombre = "Cristhofer"},
+                new Persona {nombre = "Ricardo"},
+                new Persona {nombre = "Jorge"}
+            };
+
+            _context.Persona.AddRange(personas);
+
+            var datic = new[]
+            {
+                new Datic
+                {
+                    correoInstitucional = "cris@estudiantec.cr",
+                    contrasena = "123456"
+                }
+            };
+
+            _context.Datic.AddRange(datic);
+
+            _context.SaveChanges();
+        }
+
+
+        [Fact]
+        public async Task ConfirmacionDatic()
+        {
+            //Arrange
+            var cls = new SqlDaticRepo(_context);
+
+            //Act
+            string correoInstitucional = "cris@estudiantec.cr";
+            var result = cls.GetByCorreo(correoInstitucional);
+
+            //Assert
+            string contrasena = "123456";
+            Assert.Equal(result.contrasena, contrasena);
+
+        }
+
+
+
+
+
+
+
+
+
+
+        public void Dispose()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+        }
+
+
+
+
+
+        /*
+        [Fact]
+        public void Test1()
+        {
+            var options = new DbContextOptionsBuilder<MercaditoTECContext>()
+                .UseInMemoryDatabase(databaseName: "Test1")
+                .Options;
+
+            var context = new MercaditoTECContext(options);
+
+            var _personaRepo = new SqlPersonaRepo(context);
+
+            var result = _personaRepo.GetAll();
+
+            //Esto es del ejemplo
+            Assert.Equal(5, result.Count());
+        }*/
+
+
+
+
+    }
+}
